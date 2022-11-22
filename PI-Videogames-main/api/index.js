18fe256ad3ddfea129eeb47fb76ad64c0848188b
Botsi1 <bmoreiro@gmail.com>
2022-11-22 19:04:18 -0300

    Cambios en el servidor

--- PI-Videogames-main/api/index.js
+++ PI-Videogames-main/api/index.js
@@ -20,12 +20,13 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const {db} = require("./src/Controllers/Genres/DB")
+const port = process.env.PORT || 3001
 
 // Syncing all the models at once.
 conn.sync({ force: false}).then(() => {
   db()
-  server.listen(3001, () => {
+  server.listen(port, () => {
 
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+    console.log(`%s listening at ${port}`); // eslint-disable-line no-console
   });
 });
